12f7c291d8d6b9e1e54336b9212716f1868eee27
Will Thompson <resiak@soc.pidgin.im>
2007-07-10 16:18:09 +0000

    Remove nullclient cruft from main.c; use a temporary prefs dir.
    20070710161809-984d1-622ff8fe21e3412bf70024c2e453fa9fc226a8db.gz

--- src/defines.h
+++ src/defines.h
@@ -1,4 +1,3 @@
-#define CUSTOM_USER_DIRECTORY  "/dev/null"
 #define CUSTOM_PLUGIN_PATH     ""
 #define PLUGIN_SAVE_PREF       "/purple/haze/plugins/saved"
 #define UI_ID                  "haze"
--- src/main.c
+++ src/main.c
@@ -41,6 +41,7 @@
 
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "defines.h"
 
@@ -49,10 +50,7 @@
 
 #include "connection-manager.h"
 
-/**
- * The following eventloop functions are used in both pidgin and purple-text. If your
- * application uses glib mainloop, you can safely use this verbatim.
- */
+/* Copied verbatim from nullclient.  */
 #define PURPLE_GLIB_READ_COND  (G_IO_IN | G_IO_HUP | G_IO_ERR)
 #define PURPLE_GLIB_WRITE_COND (G_IO_OUT | G_IO_HUP | G_IO_ERR | G_IO_NVAL)
 
@@ -122,37 +120,12 @@ static PurpleEventLoopUiOps glib_eventloops =
 };
 /*** End of the eventloop functions. ***/
 
-/*** Conversation uiops ***/
-static void
-null_write_conv(PurpleConversation *conv, const char *who, const char *alias,
-			const char *message, PurpleMessageFlags flags, time_t mtime)
-{
-	const char *name;
-	if (alias && *alias)
-		name = alias;
-	else if (who && *who)
-		name = who;
-	else
-		name = NULL;
-
-	printf("(%s) %s %s: %s\n", purple_conversation_get_name(conv),
-			purple_utf8_strftime("(%H:%M:%S)", localtime(&mtime)),
-			name, message);
-}
-
-static PurpleConversationUiOps haze_conv_uiops = 
-{
-	.write_conv = null_write_conv
-};
-
 static void
 haze_ui_init ()
 {
-	/**
-	 * This should initialize the UI components for all the modules. Here we
-	 * just initialize the UI for conversations.
-	 */
-	purple_conversations_set_ui_ops(&haze_conv_uiops);
+    /* We don't actually have any UI ops for anything besides debug, core and
+     * eventloop...
+     */
 }
 
 static char *debug_level_names[] =
@@ -200,7 +173,7 @@ haze_debug_init()
     purple_debug_set_ui_ops(&haze_debug_uiops);
 }
 
-static PurpleCoreUiOps null_core_uiops = 
+static PurpleCoreUiOps haze_core_uiops = 
 {
     NULL,
     haze_debug_init,
@@ -217,53 +190,35 @@ static PurpleCoreUiOps null_core_uiops =
 static void
 init_libpurple()
 {
-	/* Set a custom user directory (optional) */
-	purple_util_set_user_dir(CUSTOM_USER_DIRECTORY);
+    /* FIXME: clean up this directory on close. */
+    char *user_dir = g_strconcat (g_get_tmp_dir (), G_DIR_SEPARATOR_S,
+                                  "haze-XXXXXX", NULL);
+    
+    if (!mkdtemp (user_dir)) {
+        g_error ("Couldn't make temporary conf directory: %s",
+                 strerror (errno));
+    }
+    
+    purple_util_set_user_dir (user_dir);
 
-	/* We do not want any debugging for now to keep the noise to a minimum. */
-	purple_debug_set_enabled(FALSE);
+    /* Disable spewing debug information directly to the terminal.  The debug
+     * uiops deal with it.
+     */
+    purple_debug_set_enabled(FALSE);
 
-	/* Set the core-uiops, which is used to
-	 * 	- initialize the ui specific preferences.
-	 * 	- initialize the debug ui.
-	 * 	- initialize the ui components for all the modules.
-	 * 	- uninitialize the ui components for all the modules when the core terminates.
-	 */
-	purple_core_set_ui_ops(&null_core_uiops);
+    purple_core_set_ui_ops(&haze_core_uiops);
 
-	/* Set the uiops for the eventloop. If your client is glib-based, you can safely
-	 * copy this verbatim. */
-	purple_eventloop_set_ui_ops(&glib_eventloops);
+    purple_eventloop_set_ui_ops(&glib_eventloops);
 
-	/* Set path to search for plugins. The core (libpurple) takes care of loading the
-	 * core-plugins, which includes the protocol-plugins. So it is not essential to add
-	 * any path here, but it might be desired, especially for ui-specific plugins. */
-	purple_plugins_add_search_path(CUSTOM_PLUGIN_PATH);
+    if (!purple_core_init(UI_ID)) {
+        g_error ("libpurple initialization failed. Dumping core."
+                 "Please report this!");
+    }
 
-	/* Now that all the essential stuff has been set, let's try to init the core. It's
-	 * necessary to provide a non-NULL name for the current ui to the core. This name
-	 * is used by stuff that depends on this ui, for example the ui-specific plugins. */
-	if (!purple_core_init(UI_ID)) {
-		/* Initializing the core failed. Terminate. */
-		fprintf(stderr,
-				"libpurple initialization failed. Dumping core.\n"
-				"Please report this!\n");
-		abort();
-	}
+    purple_set_blist(purple_blist_new());
+    purple_blist_load();
 
-	/* Create and load the buddylist. */
-	purple_set_blist(purple_blist_new());
-	purple_blist_load();
-
-	/* Load the preferences. */
-	purple_prefs_load();
-
-	/* Load the desired plugins. The client should save the list of loaded plugins in
-	 * the preferences using purple_plugins_save_loaded(PLUGIN_SAVE_PREF) */
-	purple_plugins_load_saved(PLUGIN_SAVE_PREF);
-
-	/* Load the pounces. */
-	purple_pounces_load();
+    purple_prefs_load();
 }
 
 static TpBaseConnectionManager *
