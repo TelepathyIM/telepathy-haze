dcc3a08a6650457c71bef6b3e8f952d05da74809
Will Thompson <resiak@soc.pidgin.im>
2007-08-01 23:33:59 +0000

    Add .manager and .service; fix up some autofoo.
    I stole a lot of the autotools stuff in this patch verbatim from
    telepathy-gabble, and don't really understand it.  :-)  I tried and
    failed to make gabble's AS_AC_EXPAND macro work, but it seems like I
    can achieve the same goal with $(datadir)/dbus-1/services directly in
    the relevant Makefile.am.
    
    20070801233359-984d1-3ec7025633a8c59e8f563284b4c476c06351873c.gz

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,24 @@
-aclocal || exit
-autoconf || exit
-automake --add-missing --copy || exit
-./configure $@
+#!/bin/sh
+set -e
+
+if test -z "$MAKE"
+then
+	MAKE=make
+fi
+
+autoreconf -i
+
+run_configure=true
+for arg in $*; do
+    case $arg in
+        --no-configure)
+            run_configure=false
+            ;;
+        *)
+            ;;
+    esac
+done
+
+if test $run_configure = true; then
+    ./configure "$@"
+fi
