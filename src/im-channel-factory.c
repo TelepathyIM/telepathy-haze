273df6b90ba0c9cc2d88080a90d113607d20506f
Will Thompson <resiak@soc.pidgin.im>
2007-08-06 12:30:30 +0000

    conversation_{created,destroyed} uiops
    20070806123030-984d1-200c954db6099abceab0c6f5c28ff8c7961d2323.gz

--- src/im-channel-factory.c
+++ src/im-channel-factory.c
@@ -401,33 +401,45 @@ haze_write_im (PurpleConversation *conv,
     g_free (message);
 }
 
+static void
+haze_create_conversation (PurpleConversation *conv)
+{
+    g_debug ("(PurpleConversation *)%p created", conv);
+}
+
+static void
+haze_destroy_conversation (PurpleConversation *conv)
+{
+    g_debug ("(PurpleConversation *)%p destroyed", conv);
+}
+
 static PurpleConversationUiOps
 conversation_ui_ops =
 {
-    NULL,          /* create_conversation */
-    NULL,          /* destroy_conversation */
-    NULL,          /* write_chat */
-    haze_write_im, /* write_im */
-    NULL,          /* write_conv */
-    NULL,          /* chat_add_users */
-    NULL,          /* chat_rename_user */
-    NULL,          /* chat_remove_users */
-    NULL,          /* chat_update_user */
+    haze_create_conversation,  /* create_conversation */
+    haze_destroy_conversation, /* destroy_conversation */
+    NULL,                      /* write_chat */
+    haze_write_im,             /* write_im */
+    NULL,                      /* write_conv */
+    NULL,                      /* chat_add_users */
+    NULL,                      /* chat_rename_user */
+    NULL,                      /* chat_remove_users */
+    NULL,                      /* chat_update_user */
 
-    NULL,          /* present */
+    NULL,                      /* present */
 
-    NULL,          /* has_focus */
+    NULL,                      /* has_focus */
 
-    NULL,          /* custom_smiley_add */
-    NULL,          /* custom_smiley_write */
-    NULL,          /* custom_smiley_close */
+    NULL,                      /* custom_smiley_add */
+    NULL,                      /* custom_smiley_write */
+    NULL,                      /* custom_smiley_close */
 
-    NULL,          /* send_confirm */
+    NULL,                      /* send_confirm */
 
-    NULL,          /* _purple_reserved1 */
-    NULL,          /* _purple_reserved2 */
-    NULL,          /* _purple_reserved3 */
-    NULL,          /* _purple_reserved4 */
+    NULL,                      /* _purple_reserved1 */
+    NULL,                      /* _purple_reserved2 */
+    NULL,                      /* _purple_reserved3 */
+    NULL,                      /* _purple_reserved4 */
 };
 
 PurpleConversationUiOps *
