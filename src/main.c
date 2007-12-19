324fd80e96bd4f428fbcea0e18d5939ad1c3f17f
Will Thompson <will@willthompson.co.uk>
2007-12-19 11:33:40 +0000

    Add --enable-leaky-request-stubs to configure, defaulting to off.
    20071219113340-4210b-78b79d8797518e7ec3574aa72bc63be332a29ef6.gz

--- src/main.c
+++ src/main.c
@@ -157,7 +157,9 @@ haze_ui_init ()
     purple_accounts_set_ui_ops (haze_get_account_ui_ops ());
     purple_conversations_set_ui_ops (haze_get_conv_ui_ops ());
     purple_connections_set_ui_ops (haze_get_connection_ui_ops ());
+#ifdef LEAKY_REQUEST_STUBS
     purple_request_set_ui_ops (haze_request_get_ui_ops ());
+#endif
 }
 
 static PurpleCoreUiOps haze_core_uiops = 
