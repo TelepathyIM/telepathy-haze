c91431ca034af2262f6029a33d7052c4d9446515
Simon McVittie <http://smcv.pseudorandom.co.uk/>
2008-10-16 09:36:15 +0100

    Use automake 1.9 (as in Gabble and telepathy-glib)

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 set -e
 
-if test -z "$MAKE"
-then
-	MAKE=make
+if test -n "$AUTOMAKE"; then
+    : # don't override an explicit user request
+elif automake-1.9 --version >/dev/null 2>/dev/null && \
+     aclocal-1.9 --version >/dev/null 2>/dev/null; then
+    # If we have automake-1.9, use it. This helps to ensure that our build
+    # system doesn't accidentally grow automake-1.10 dependencies.
+    AUTOMAKE=automake-1.9
+    export AUTOMAKE
+    ACLOCAL=aclocal-1.9
+    export ACLOCAL
 fi
 
-autoreconf -i
+autoreconf -i -f
 
 run_configure=true
 for arg in $*; do
