da60485da4ac62f6d9909f62e620f6940ae5a3b4
Mike Ruprecht <mike.ruprecht@collabora.co.uk>
2009-10-15 19:52:15 -0500

    Implement StreamedMedia channels.

--- src/media-manager.c
+++ src/media-manager.c
@@ -28,6 +28,7 @@
 
 #include "connection.h"
 #include "debug.h"
+#include "media-channel.h"
 #include "mediamanager.h"
 
 static void channel_manager_iface_init (gpointer, gpointer);
@@ -153,17 +154,86 @@ haze_media_manager_class_init (HazeMediaManagerClass *haze_media_manager_class)
 
 }
 
+/**
+ * media_channel_closed_cb:
+ *
+ * Signal callback for when a media channel is closed. Removes the references
+ * that #HazeMediaManager holds to them.
+ */
+static void
+media_channel_closed_cb (HazeMediaChannel *chan, gpointer user_data)
+{
+  HazeMediaManager *self = HAZE_MEDIA_MANAGER (user_data);
+  HazeMediaManagerPrivate *priv = self->priv;
+
+  tp_channel_manager_emit_channel_closed_for_object (self,
+      TP_EXPORTABLE_CHANNEL (chan));
+
+  DEBUG ("removing media channel %p with ref count %d",
+      chan, G_OBJECT (chan)->ref_count);
+
+  g_ptr_array_remove (priv->channels, chan);
+  g_object_unref (chan);
+}
+
+/**
+ * new_media_channel
+ *
+ * Creates a new empty HazeMediaChannel.
+ */
+static HazeMediaChannel *
+new_media_channel (HazeMediaManager *self,
+                   PurpleMedia *media,
+                   TpHandle maybe_peer,
+                   gboolean peer_in_rp,
+                   gboolean initial_audio,
+                   gboolean initial_video)
+{
+  HazeMediaManagerPrivate *priv;
+  TpBaseConnection *conn;
+
+  HazeMediaChannel *chan;
+  gchar *object_path;
+
+  g_assert (HAZE_IS_MEDIA_MANAGER (self));
+
+  priv = self->priv;
+  conn = (TpBaseConnection *) priv->conn;
+
+  object_path = g_strdup_printf ("%s/MediaChannel%u",
+      conn->object_path, priv->channel_index);
+  priv->channel_index += 1;
+
+  chan = g_object_new (HAZE_TYPE_MEDIA_CHANNEL,
+                       "connection", priv->conn,
+                       "object-path", object_path,
+                       "media", media,
+                       "initial-peer", maybe_peer,
+                       "peer-in-rp", peer_in_rp,
+                       "initial-audio", initial_audio,
+                       "initial-video", initial_video,
+                       NULL);
+
+  DEBUG ("object path %s", object_path);
+
+  g_signal_connect (chan, "closed", (GCallback) media_channel_closed_cb, self);
+
+  g_ptr_array_add (priv->channels, chan);
+
+  g_free (object_path);
+
+  return chan;
+}
+
 static void
 haze_media_manager_close_all (HazeMediaManager *self)
 {
   HazeMediaManagerPrivate *priv = self->priv;
-  /* TODO: uncomment this when there's a channel class */
-#if 0
-  GPtrArray *ret = g_ptr_array_sized_new (priv->channels->len);
+  GPtrArray *tmp = g_ptr_array_sized_new (priv->channels->len);
   guint i;
 
   for (i = 0; i < priv->channels->len; i++)
-    g_ptr_array_add (ret, g_ptr_array_index (priv->channels, i));
+    g_ptr_array_add (tmp, g_ptr_array_index (priv->channels, i));
 
   DEBUG ("closing channels");
 
@@ -174,7 +244,7 @@ haze_media_manager_close_all (HazeMediaManager *self)
       DEBUG ("closing %p", chan);
       haze_media_channel_close (chan);
     }
-#endif
+
   if (priv->status_changed_id != 0)
     {
       g_signal_handler_disconnect (priv->conn,
@@ -190,9 +260,22 @@ init_media_cb (PurpleMediaManager *manager,
                const gchar *username,
                HazeMediaManager *self)
 {
-  /* TODO: flesh out this function */
-  DEBUG ("init_media");
-  return FALSE;
+  HazeMediaManagerPrivate *priv = self->priv;
+  TpBaseConnection *base_conn = TP_BASE_CONNECTION (priv->conn);
+  TpHandleRepoIface *contact_repo =
+      tp_base_connection_get_handles (base_conn, TP_HANDLE_TYPE_CONTACT);
+  TpHandle contact = tp_handle_ensure (contact_repo, username, NULL, NULL);
+  HazeMediaChannel *chan;
+
+  if (purple_media_is_initiator (media, NULL, NULL) == TRUE)
+    return TRUE;
+
+  chan = new_media_channel (self, media, contact,
+      FALSE, FALSE, FALSE);
+  tp_channel_manager_emit_new_channel (self,
+      TP_EXPORTABLE_CHANNEL (chan), NULL);
+  DEBUG ("called");
+  return TRUE;
 }
 
 static void
@@ -249,11 +332,23 @@ haze_media_manager_foreach_channel (TpChannelManager *manager,
     }
 }
 
+static const gchar * const media_channel_fixed_properties[] = {
+    TP_IFACE_CHANNEL ".ChannelType",
+    TP_IFACE_CHANNEL ".TargetHandleType",
+    NULL
+};
+
 static const gchar * const named_channel_allowed_properties[] = {
     TP_IFACE_CHANNEL ".TargetHandle",
     TP_IFACE_CHANNEL ".TargetID",
     TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA ".InitialAudio",
     TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA ".InitialVideo",
+    NULL
+};
+
+/* not advertised in foreach_channel_class - can only be requested with
+ * RequestChannel, not with CreateChannel/EnsureChannel */
+static const gchar * const anon_channel_allowed_properties[] = {
     NULL
 };
 
@@ -287,19 +382,196 @@ typedef enum
   METHOD_ENSURE,
 } RequestMethod;
 
+typedef struct
+{
+    HazeMediaManager *self;
+    HazeMediaChannel *channel;
+    gpointer request_token;
+} MediaChannelRequest;
+
+static MediaChannelRequest *
+media_channel_request_new (HazeMediaManager *self,
+    HazeMediaChannel *channel,
+    gpointer request_token)
+{
+  MediaChannelRequest *mcr = g_slice_new0 (MediaChannelRequest);
+
+  mcr->self = self;
+  mcr->channel = channel;
+  mcr->request_token = request_token;
+
+  return mcr;
+}
+
+static void
+media_channel_request_free (MediaChannelRequest *mcr)
+{
+  g_slice_free (MediaChannelRequest, mcr);
+}
+
+static void
+media_channel_request_succeeded_cb (MediaChannelRequest *mcr,
+    GPtrArray *streams)
+{
+  GSList *request_tokens;
+
+  request_tokens = g_slist_prepend (NULL, mcr->request_token);
+  tp_channel_manager_emit_new_channel (mcr->self,
+      TP_EXPORTABLE_CHANNEL (mcr->channel), request_tokens);
+  g_slist_free (request_tokens);
+
+  media_channel_request_free (mcr);
+}
+
+static void
+media_channel_request_failed_cb (MediaChannelRequest *mcr,
+    GError *error)
+{
+  tp_channel_manager_emit_request_failed (mcr->self, mcr->request_token,
+      error->domain, error->code, error->message);
+
+  media_channel_request_free (mcr);
+}
+
 static gboolean
 haze_media_manager_requestotron (TpChannelManager *manager,
                                  gpointer request_token,
                                  GHashTable *request_properties,
                                  RequestMethod method)
 {
-  /* TODO: Implement the rest of this once there's a channel class */
+  HazeMediaManager *self = HAZE_MEDIA_MANAGER (manager);
+  HazeMediaManagerPrivate *priv = self->priv;
+  TpHandleType handle_type;
+  TpHandle handle;
+  HazeMediaChannel *channel = NULL;
+  GError *error = NULL;
+  gboolean require_target_handle, add_peer_to_remote_pending;
+  gboolean initial_audio, initial_video;
+
+  /* Supported modes of operation:
+   * - RequestChannel(None, 0):
+   *     channel is anonymous;
+   *     caller may optionally use AddMembers to add the peer to RemotePending
+   *     without sending them any XML;
+   *     caller uses RequestStreams to set the peer and start the call.
+   * - RequestChannel(Contact, n) where n != 0:
+   *     channel has TargetHandle=n;
+   *     n is in remote pending;
+   *     call is started when caller calls RequestStreams.
+   * - CreateChannel({THT: Contact, TH: n}):
+   *     channel has TargetHandle=n
+   *     n is not in the group interface at all
+   *     call is started when caller calls RequestStreams.
+   * - EnsureChannel({THT: Contact, TH: n}):
+   *     look for a channel whose peer is n, and return that if found with
+   *       whatever properties and group membership it has;
+   *     otherwise the same as into CreateChannel
+   */
+  switch (method)
+    {
+    case METHOD_REQUEST:
+      require_target_handle = FALSE;
+      add_peer_to_remote_pending = TRUE;
+      break;
+    case METHOD_CREATE:
+    case METHOD_ENSURE:
+      require_target_handle = TRUE;
+      add_peer_to_remote_pending = FALSE;
+      break;
+    default:
+      g_assert_not_reached ();
+    }
+
   if (tp_strdiff (tp_asv_get_string (request_properties,
-    TP_IFACE_CHANNEL ".ChannelType"),
-      TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA))
+          TP_IFACE_CHANNEL ".ChannelType"),
+        TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA))
     return FALSE;
 
-  return FALSE;
+  handle_type = tp_asv_get_uint32 (request_properties,
+      TP_IFACE_CHANNEL ".TargetHandleType", NULL);
+
+  handle = tp_asv_get_uint32 (request_properties,
+      TP_IFACE_CHANNEL ".TargetHandle", NULL);
+
+  initial_audio = tp_asv_get_boolean (request_properties,
+      TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA ".InitialAudio", NULL);
+  initial_video = tp_asv_get_boolean (request_properties,
+      TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA ".InitialVideo", NULL);
+
+  switch (handle_type)
+    {
+    case TP_HANDLE_TYPE_NONE:
+      /* already checked by TpBaseConnection */
+      g_assert (handle == 0);
+
+      if (require_target_handle)
+        {
+          g_set_error (&error, TP_ERRORS, TP_ERROR_NOT_IMPLEMENTED,
+              "A valid Contact handle must be provided when requesting a media "
+              "channel");
+          goto error;
+        }
+
+      if (tp_channel_manager_asv_has_unknown_properties (request_properties,
+              media_channel_fixed_properties, anon_channel_allowed_properties,
+              &error))
+        goto error;
+
+      channel = new_media_channel (self, NULL, 0, FALSE, FALSE, FALSE);
+      break;
+
+    case TP_HANDLE_TYPE_CONTACT:
+      /* validity already checked by TpBaseConnection */
+      g_assert (handle != 0);
+
+      if (tp_channel_manager_asv_has_unknown_properties (request_properties,
+              media_channel_fixed_properties, named_channel_allowed_properties,
+              &error))
+        goto error;
+
+      if (method == METHOD_ENSURE)
+        {
+          guint i;
+          TpHandle peer = 0;
+
+          for (i = 0; i < priv->channels->len; i++)
+            {
+              channel = g_ptr_array_index (priv->channels, i);
+              g_object_get (channel, "peer", &peer, NULL);
+
+              if (peer == handle)
+                {
+                  /* Per the spec, we ignore InitialAudio and InitialVideo when
+                   * looking for an existing channel.
+                   */
+                  tp_channel_manager_emit_request_already_satisfied (self,
+                      request_token, TP_EXPORTABLE_CHANNEL (channel));
+                  return TRUE;
+                }
+            }
+        }
+
+      channel = new_media_channel (self, NULL, handle,
+          add_peer_to_remote_pending, initial_audio, initial_video);
+      break;
+    default:
+      return FALSE;
+    }
+
+  g_assert (channel != NULL);
+
+  haze_media_channel_request_initial_streams (channel,
+      (GFunc) media_channel_request_succeeded_cb,
+      (GFunc) media_channel_request_failed_cb,
+      media_channel_request_new (self, channel, request_token));
+
+  return TRUE;
+
+error:
+  tp_channel_manager_emit_request_failed (self, request_token,
+      error->domain, error->code, error->message);
+  g_error_free (error);
+  return TRUE;
 }
 
 static gboolean
